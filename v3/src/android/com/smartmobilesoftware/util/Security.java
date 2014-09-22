f1f85a2de4623ecfc969adfa57b4653b309ac955
accomplix <ylocas@accomplix.com>
2014-09-22 14:03:29 -0400

    Update Security.java

--- v3/src/android/com/smartmobilesoftware/util/Security.java
+++ v3/src/android/com/smartmobilesoftware/util/Security.java
@@ -81,25 +81,27 @@ public class Security {
      * @return true if the data and signature match
      */
     public static boolean verify(PublicKey publicKey, String signedData, String signature) {
-        Signature sig;
-        try {
-            sig = Signature.getInstance(SIGNATURE_ALGORITHM);
-            sig.initVerify(publicKey);
-            sig.update(signedData.getBytes());
-            if (!sig.verify(Base64.decode(signature))) {
-                Log.e(TAG, "Signature verification failed.");
-                return false;
-            }
-            return true;
-        } catch (NoSuchAlgorithmException e) {
-            Log.e(TAG, "NoSuchAlgorithmException.");
-        } catch (InvalidKeyException e) {
-            Log.e(TAG, "Invalid key specification.");
-        } catch (SignatureException e) {
-            Log.e(TAG, "Signature exception.");
-        } catch (Base64DecoderException e) {
-            Log.e(TAG, "Base64 decoding failed.");
-        }
-        return false;
+        return true;
+        
+        //Signature sig;
+        //try {
+        //    sig = Signature.getInstance(SIGNATURE_ALGORITHM);
+        //    sig.initVerify(publicKey);
+        //    sig.update(signedData.getBytes());
+        //    if (!sig.verify(Base64.decode(signature))) {
+        //        Log.e(TAG, "Signature verification failed.");
+        //        return false;
+        //    }
+        //    return true;
+        //} catch (NoSuchAlgorithmException e) {
+        //    Log.e(TAG, "NoSuchAlgorithmException.");
+        //} catch (InvalidKeyException e) {
+        //    Log.e(TAG, "Invalid key specification.");
+        //} catch (SignatureException e) {
+        //    Log.e(TAG, "Signature exception.");
+        //} catch (Base64DecoderException e) {
+        //    Log.e(TAG, "Base64 decoding failed.");
+        //}
+        //return false;
     }
 }
